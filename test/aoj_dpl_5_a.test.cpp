d83ab2d38871f1e191221853e2a85ed2b5ddeb8d
firiexp <firizfiriz300@hotmail.co.jp>
2020-04-27 22:39:47 +0900

    add verify pow

---
+++ test/aoj_dpl_5_a.test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <bitset>
+#include <cmath>
+
+static const int MOD = 1000000007;
+using ll = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+using namespace std;
+
+template<class T> constexpr T INF = ::numeric_limits<T>::max()/32*15+208;
+
+#include "../util/modint.cpp"
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << mint(k).pow(n).val << "\n";
+    return 0;
+}
