6c9a1d1f153d3da879da8adcd216da2d12122e32
firiexp <firizfiriz300@hotmail.co.jp>
2020-04-28 18:52:24 +0900

    fix

--- test/aoj0335.test.cpp
+++ test/aoj0335.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0335"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0324"
 #include <iostream>
 #include <algorithm>
 #include <map>
