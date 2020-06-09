f4d5b96f093c0d0724c58bed1e89d73cb7510ca2
firiexp <firizfiriz300@hotmail.co.jp>
2020-06-09 14:40:12 +0900

    add SCC test

--- graph/SCC.cpp
+++ graph/SCC.cpp
@@ -1,14 +1,4 @@
-struct SCC {
-    vector<vector<int>> G, G_r, G_out;
-    vector<int> vs, used, cmp;
-    SCC() = default;
-    explicit SCC(int n) : G(n), G_r(n), G_out(n), used(n), cmp(n) {}
-
-    void add_edge(int a, int b){
-        G[a].emplace_back(b);
-        G_r[b].emplace_back(a);
-    }
-
+class SCC {
     void dfs(int v){
         used[v] = 1;
         for (auto &&u : G[v]) if(!used[u]) dfs(u);
@@ -18,7 +8,18 @@ struct SCC {
     void dfs_r(int v, int k){
         used[v] = 1;
         cmp[v] = k;
+        sz[k]++;
         for (auto &&u : G_r[v]) if(!used[u]) dfs_r(u, k);
+    }
+public:
+    vector<vector<int>> G, G_r, G_out;
+    vector<int> vs, used, cmp, sz;
+    SCC() = default;
+    explicit SCC(int n) : G(n), G_r(n), G_out(n), used(n), cmp(n), sz(n) {}
+
+    void add_edge(int a, int b){
+        G[a].emplace_back(b);
+        G_r[b].emplace_back(a);
     }
 
     int build() {
@@ -32,6 +33,7 @@ struct SCC {
             }
         }
         G_out.resize(k);
+        sz.resize(k);
         for (int i = 0; i < n; ++i) {
             for (auto &&j : G[i]) {
                 if(cmp[i] != cmp[j]){
@@ -39,9 +41,9 @@ struct SCC {
                 }
             }
         }
-        for (int i = 0; i < k; ++i) {
-            sort(G_out.begin(), G_out.end());
-            G_out.erase(unique(G_out.begin(), G_out.end()), G_out.end());
+        for (auto &&l : G_out) {
+            sort(l.begin(), l.end());
+            l.erase(unique(l.begin(), l.end()), l.end());
         }
         return k;
     }
---
+++ test/aoj0349.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0349"
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <bitset>
+#include <cmath>
+
+static const int MOD = 1000000007;
+using ll = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+using namespace std;
+
+template<class T> constexpr T INF = ::numeric_limits<T>::max()/32*15+208;
+
+#include "../graph/SCC.cpp"
+
+int main() {
+    int n;
+    cin >> n;
+    SCC G(n);
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        if(x%n == 0) ans++;
+        else G.add_edge(i, (i+x)%n);
+    }
+    G.build();
+
+    for (auto &&j : G.sz) {
+        if(j != 1) ans += j;
+    }
+    cout << ans << "\n";
+    return 0;
+}
