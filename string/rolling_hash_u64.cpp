0827142cdca28ec938cf3c6b3e3290c0c361768b
firiexp <firizfiriz300@hotmail.co.jp>
2020-10-20 21:39:42 +0900

    fix roriha

--- string/rolling_hash_u64.cpp
+++ string/rolling_hash_u64.cpp
@@ -6,7 +6,7 @@ constexpr u64 MASK31 = (1UL << 31) - 1;
 
 class rolling_hash_u64 {
     static u64 get_base(){
-        u64 z = (static_cast<uint64_t>((chrono::system_clock::now().time_since_epoch().count())&((1LL << 32)-1)))+0x9e3779b97f4a7c15;
+        u64 z = static_cast<uint64_t>(chrono::system_clock::now().time_since_epoch().count())+0x9e3779b97f4a7c15;
         z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
         z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
         return z;
@@ -16,6 +16,13 @@ class rolling_hash_u64 {
         static u64 B_ = (get_base())%(M-2)+2;
         return B_;
     }
+
+    static inline u64 calc_mod(u64 val){
+        val = (val & M) + (val >> 61);
+        if(val > M) val -= M;
+        return val;
+    }
+public:
     static vector<u64> &p() {
         static vector<u64> p_{1, B()};
         return p_;
@@ -26,14 +33,8 @@ class rolling_hash_u64 {
         return (a*c << 1) + b*d + ((e & MASK30) << 31) + (e >> 30);
     }
 
-    static inline u64 calc_mod(u64 val){
-        val = (val & M) + (val >> 61);
-        if(val > M) val -= M;
-        return val;
-    }
-public:
     vector<u64> hash;
-    explicit rolling_hash_u64(const string &s) {
+    rolling_hash_u64(const string &s) {
         if(p().size() <= s.size()){
             int l = p().size();
             p().resize(s.size()+1);
@@ -47,8 +48,15 @@ public:
         }
     };
 
+    rolling_hash_u64(const int& n){
+        int l = p().size();
+        p().resize(n+1);
+        for (int i = l; i < p().size(); ++i) {
+            p()[i] = calc_mod(mul(p()[i-1], p()[1]));
+        }
+    }
+
     u64 get(int l, int r){
         return calc_mod(hash[r] + POSITIVISER - mul(hash[l], p()[r-l]));
     }
 };
-
