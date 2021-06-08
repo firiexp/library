5cc976329089fb5e245f1768dcba7fe956d0ad9a
firiexp <firizfiriz300@hotmail.co.jp>
2021-06-08 23:33:29 +0900

    add SA test

---
+++ test/yosupo_suffix_array.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/suffixarray"
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <bitset>
+#include <cmath>
+
+static const int MOD = 1000000007;
+using ll = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+using namespace std;
+
+template<class T> constexpr T INF = ::numeric_limits<T>::max()/32*15+208;
+
+
+#include "../string/suffix_array.cpp"
+
+int main() {
+    string s;
+    cin >> s;
+    auto ans = suffix_array(s);
+    for (int i = 0; i < ans.size(); ++i) {
+        if(i) printf(" ");
+        printf("%d", ans[i]);
+    }
+    puts("");
+    return 0;
+}
