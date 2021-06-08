dda486927c510e997dc8839ba2ec2e67b7005088
firiexp <firizfiriz300@hotmail.co.jp>
2021-06-09 00:29:05 +0900

    add tetration test

---
+++ test/yosupo_tetration_mod.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tetration_mod"
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <bitset>
+#include <cmath>
+
+static const int MOD = 1000000007;
+using ll = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+using namespace std;
+
+template<class T> constexpr T INF = ::numeric_limits<T>::max() / 32 * 15 + 208;
+
+#include "../math/tetration.cpp"
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, m;
+        scanf("%d %d %d", &a, &b, &m);
+        printf("%lld\n", tetration(a, b, m));
+    }
+    return 0;
+}
