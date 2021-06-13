3ca9cfb680d6d8131edb617341036283fafea683
firiexp <firizfiriz300@hotmail.co.jp>
2021-06-13 09:35:11 +0900

    fix binarytrie

--- datastructure/binarytrie.cpp
+++ datastructure/binarytrie.cpp
@@ -3,10 +3,10 @@ struct Node {
     Node *nxt[2];
     Node() : val(0), nxt{nullptr, nullptr} {}
     static int cnt;
-    void *operator new(size_t){
-        static Node pool[3000000];
+    /* void *operator new(size_t){
+        static Node pool[6000000];
         return pool + cnt++;
-    }
+    }*/
 };
 int Node::cnt = 0;
 
@@ -27,13 +27,9 @@ struct Binarytrie {
 
     T xor_min(Node* t, const T &x, int dep = X-1){
         if(dep < 0) return 0;
-        T f = (x >> dep) & 1U; f ^= !t->nxt[f];
+        T f = (x >> dep) & 1; f ^= !t->nxt[f];
         return xor_min(t->nxt[f], x, dep-1) | (f << dep);
     }
-    T max_element(T x = 0) {
-        return xor_min(root, ~x);
-    }
-    T min_element(T x = 0){
-        return xor_min(root, x);
-    }
+    T max_element(T x = 0) { return xor_min(root, ~x)^x; }
+    T min_element(T x = 0){ return xor_min(root, x)^x; }
 };
