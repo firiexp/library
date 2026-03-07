a5175d156d5bad69200ee8a8449534ea628f7a8a
firiexp <firizfiriz300@hotmail.co.jp>
2026-03-08 01:10:05 +0900

    fix maxflow_lower_bound

--- graph/maxflow_lower_bound.cpp
+++ graph/maxflow_lower_bound.cpp
@@ -1,76 +1,7 @@
+#include "../flow/dinic.cpp"
+
 template<class T>
 class MaxFlowLowerBound {
-    struct Dinic {
-        struct edge {
-            int to{};
-            T cap{};
-            int rev{};
-            edge() = default;
-            edge(int to, T cap, int rev) : to(to), cap(cap), rev(rev) {}
-        };
-
-        vector<vector<edge>> G;
-        vector<int> level, iter;
-
-        Dinic() = default;
-        explicit Dinic(int n) : G(n), level(n), iter(n) {}
-
-        pair<int, int> add_edge(int from, int to, T cap) {
-            int fi = (int)G[from].size();
-            int ti = (int)G[to].size();
-            G[from].emplace_back(to, cap, ti);
-            G[to].emplace_back(from, 0, fi);
-            return {from, fi};
-        }
-
-        void bfs(int s) {
-            fill(level.begin(), level.end(), -1);
-            queue<int> q;
-            level[s] = 0;
-            q.emplace(s);
-            while(!q.empty()) {
-                int v = q.front();
-                q.pop();
-                for(auto &&e : G[v]) {
-                    if(e.cap > 0 && level[e.to] < 0) {
-                        level[e.to] = level[v] + 1;
-                        q.emplace(e.to);
-                    }
-                }
-            }
-        }
-
-        T dfs(int v, int t, T f) {
-            if(v == t) return f;
-            for(int &i = iter[v]; i < (int)G[v].size(); ++i) {
-                edge &e = G[v][i];
-                if(e.cap > 0 && level[v] < level[e.to]) {
-                    T d = dfs(e.to, t, min(f, e.cap));
-                    if(d == 0) continue;
-                    e.cap -= d;
-                    G[e.to][e.rev].cap += d;
-                    return d;
-                }
-            }
-            return 0;
-        }
-
-        T flow(int s, int t, T lim = INF<T>) {
-            T ret = 0;
-            while(lim > 0) {
-                bfs(s);
-                if(level[t] < 0) break;
-                fill(iter.begin(), iter.end(), 0);
-                while(lim > 0) {
-                    T f = dfs(s, t, lim);
-                    if(f == 0) break;
-                    ret += f;
-                    lim -= f;
-                }
-            }
-            return ret;
-        }
-    };
 
     struct raw_edge {
         int from{}, to{};
@@ -89,8 +20,13 @@ public:
 
     pair<bool, T> max_flow(int s, int t) {
         int ss = n, tt = n + 1;
-        Dinic mf(n + 2);
+        Dinic<T, true> mf(n + 2);
         vector<T> b(n, 0);
+        auto add_edge = [&](int from, int to, T cap) {
+            int idx = (int)mf.G[from].size();
+            mf.add_edge(from, to, cap);
+            return pair<int, int>{from, idx};
+        };
 
         for(auto &&e : edges) {
             mf.add_edge(e.from, e.to, e.upper - e.lower);
@@ -98,13 +34,13 @@ public:
             b[e.to] += e.lower;
         }
 
-        auto ts = mf.add_edge(t, s, INF<T>);
+        auto ts = add_edge(t, s, INF<T>);
         T req = 0;
         vector<pair<int, int>> super_edges;
         for(int v = 0; v < n; ++v) {
             if(b[v] > 0) {
                 req += b[v];
-                super_edges.emplace_back(mf.add_edge(ss, v, b[v]));
+                super_edges.emplace_back(add_edge(ss, v, b[v]));
             } else if(b[v] < 0) {
                 mf.add_edge(v, tt, -b[v]);
             }
