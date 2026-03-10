f254833055a4da67055335989ea5064e4f614af1
firiexp <firizfiriz300@hotmail.co.jp>
2026-03-11 00:38:22 +0900

    miller rabin 整理

--- math/miller_rabin.cpp
+++ math/miller_rabin.cpp
@@ -1,35 +1,82 @@
-template< class T>
-T pow_ (T x, uint64_t n, uint64_t M){
-    T u = 1;
-    if(n > 0){
-        u = pow_(x, n/2, M);
-        if (n % 2 == 0) u = (u*u) % M;
-        else u = (((u * u)% M) * x) % M;
+using u128 = __uint128_t;
+
+struct mod64 {
+    unsigned long long n;
+    static unsigned long long mod, inv, r2;
+    mod64() : n(0) {}
+    mod64(unsigned long long x) : n(init(x)) {}
+    static unsigned long long init(unsigned long long w) {
+        return reduce(u128(w) * r2);
     }
-    return u;
+    static void set_mod(unsigned long long m) {
+        mod = inv = m;
+        for (int i = 0; i < 5; ++i) inv *= 2 - inv * m;
+        r2 = -u128(m) % m;
+    }
+    static unsigned long long reduce(u128 x) {
+        unsigned long long y =
+            static_cast<unsigned long long>(x >> 64)
+            - static_cast<unsigned long long>((u128(static_cast<unsigned long long>(x) * inv) * mod) >> 64);
+        return (long long)y < 0 ? y + mod : y;
+    }
+    mod64& operator*=(mod64 x) {
+        n = reduce(u128(n) * x.n);
+        return *this;
+    }
+    mod64 operator*(mod64 x) const {
+        return mod64(*this) *= x;
+    }
+    mod64& operator+=(mod64 x) {
+        n += x.n - mod;
+        if((long long)n < 0) n += mod;
+        return *this;
+    }
+    mod64 operator+(mod64 x) const {
+        return mod64(*this) += x;
+    }
+    unsigned long long val() const {
+        return reduce(n);
+    }
 };
 
-bool suspect(__uint128_t a, uint64_t s, uint64_t d, uint64_t n){
-    __uint128_t x = pow_(a, d, n);
-    if (x == 1) return true;
-    for (int r = 0; r < s; ++r) {
-        if(x == n-1) return true;
-        x = x * x % n;
+unsigned long long mod64::mod, mod64::inv, mod64::r2;
+
+bool suspect(unsigned long long a, unsigned long long s, unsigned long long d, unsigned long long n){
+    if(mod64::mod != n) mod64::set_mod(n);
+    mod64 x(1), xx(a), one(1), minusone(n - 1);
+    while(d > 0){
+        if(d & 1) x *= xx;
+        xx *= xx;
+        d >>= 1;
+    }
+    if (x.n == one.n) return true;
+    for (unsigned long long r = 0; r < s; ++r) {
+        if(x.n == minusone.n) return true;
+        x *= x;
     }
     return false;
 }
 
 template<class T>
 bool miller_rabin(T m){
-    uint64_t n = m;
+    unsigned long long n = m;
     if (n <= 1 || (n > 2 && n % 2 == 0)) return false;
-    uint64_t d = n - 1, s = 0;
-    while (!(d&1)) {++s; d >>= 1;}
-    vector<uint64_t> v = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
-    if(n < 4759123141LL) v = {2, 7, 61};
-    for (auto &&p : v) {
-        if(p >= n) break;
-        if(!suspect(p, s, d, n)) return false;
+    if (n == 2 || n == 3 || n == 5 || n == 7) return true;
+    if (n % 3 == 0 || n % 5 == 0 || n % 7 == 0) return false;
+    unsigned long long d = n - 1, s = 0;
+    while (!(d & 1)) { ++s; d >>= 1; }
+    static constexpr unsigned long long small[] = {2, 7, 61};
+    static constexpr unsigned long long large[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    if(n < 4759123141ULL) {
+        for (auto p : small) {
+            if(p >= n) break;
+            if(!suspect(p, s, d, n)) return false;
+        }
+    } else {
+        for (auto p : large) {
+            if(p >= n) break;
+            if(!suspect(p, s, d, n)) return false;
+        }
     }
     return true;
 }
--- math/primefactor_ll.cpp
+++ math/primefactor_ll.cpp
@@ -1,56 +1,9 @@
 #include <algorithm>
-#include <array>
 #include <numeric>
 #include <random>
 
 using ull = unsigned long long;
-using u128 = __uint128_t;
-
-template<class T>
-T pow_(T x, ull n, ull M){
-    T r = 1;
-    while(n){
-        if(n & 1) r = (u128)r * x % M;
-        x = (u128)x * x % M;
-        n >>= 1;
-    }
-    return r;
-}
-
-bool suspect(__uint128_t a, ull s, ull d, ull n){
-    __uint128_t x = pow_(a, d, n);
-    if (x == 1) return true;
-    for (ull r = 0; r < s; ++r) {
-        if(x == n-1) return true;
-        x = x * x % n;
-    }
-    return false;
-}
-
-template<class T>
-bool miller_rabin(T m){
-    ull n = m;
-    if (n <= 1 || (n > 2 && n % 2 == 0)) return false;
-    if (n == 2 || n == 3 || n == 5 || n == 7) return true;
-    if (n % 3 == 0 || n % 5 == 0 || n % 7 == 0) return false;
-    ull d = n - 1, s = 0;
-    while (!(d&1)) {++s; d >>= 1;}
-    static constexpr array<ull, 3> small = {2, 7, 61};
-    static constexpr array<ull, 7> large = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
-    if (n <= 4759123141ULL) {
-        for (ull p : small) {
-            if(p >= n) break;
-            if(!suspect(p, s, d, n)) return false;
-        }
-    } else {
-        for (ull p : large) {
-            if(p >= n) break;
-            if(!suspect(p, s, d, n)) return false;
-        }
-    }
-    return true;
-}
-
+#include "miller_rabin.cpp"
 
 template<typename T>
 struct ExactDiv {
@@ -87,30 +40,6 @@ vector<ExactDiv<ull>> get_prime(int n){
 const auto primes = get_prime(50000);
 
 mt19937_64 rng(0x8a5cd789635d2dffULL);
-
-struct mod64 {
-    ull n;
-    static ull mod, inv, r2;
-    mod64() : n(0) {}
-    mod64(ull x) : n(init(x)) {}
-    static ull init(ull w) { return reduce(u128(w) * r2); }
-    static void set_mod(ull m) {
-        mod = inv = m;
-        for (int i = 0; i < 5; ++i) inv *= 2 - inv * m;
-        r2 = -u128(m) % m;
-    }
-    static ull reduce(u128 x) {
-        ull y = ull(x >> 64) - ull((u128(ull(x) * inv) * mod) >> 64);
-        return ll(y) < 0 ? y + mod : y;
-    };
-    mod64& operator+=(mod64 x) { n += x.n - mod; if(ll(n) < 0) n += mod; return *this; }
-    mod64 operator+(mod64 x) const { return mod64(*this) += x; }
-    mod64& operator*=(mod64 x) { n = reduce(u128(n) * x.n);  return *this; }
-    mod64 operator*(mod64 x) const { return mod64(*this) *= x; }
-    ull val() const { return reduce(n); }
-};
-
-ull mod64::mod, mod64::inv, mod64::r2;
 
 template<class T>
 T pollard_rho2(T n) {
